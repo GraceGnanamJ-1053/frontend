22ec60c6bd93844255c83923ea26980b63b2edc4
GraceGnanamJ-1053 <gracegnanam@karunya.edu.in>
2025-04-24 10:24:01 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return(
         <header className="text-center bg-purple-800 text-white py-8 px-80">
-            <h1 className="text-5xl font-semibold mb-4">Fuel Management</h1>
+            <h1 className="text-5xl font-semibold mb-4">Online Delivery</h1>
             <p className="text-xl"></p>
         </header>
     )
